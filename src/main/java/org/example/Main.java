0b8f74432b07721526f8bebb32322f0943154856
Aya <ohnoaya@ayaMac.local>
2024-11-18 21:49:19 +0900

    問題１(mission１)を変更

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,16 @@ package org.example;
 public class Main {
 
   public static void main(String[] args) {
+// 入力例:
+// 1番目の数字を入力してください: 10
+// 演算子を入力してください (+, -, *, /): +
+// 2番目の数字を入力してください: 5
+//
+// 期待される出力例:
+// 計算結果: 10 + 5 = 15
 
-
+    int num1 = 10;
+    int num2 = 5;
+    System.out.println(num1 + "+" + num2 + "=" + (num1 + num2));
   }
 }
